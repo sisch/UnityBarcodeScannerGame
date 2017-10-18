27aa4bf7c22311cedcbe0f25d0cb0d2bd09e69cf
Simon Schliesky <simon@schliesky.com>
2017-10-18 19:21:38 +0200

    Noch mehr Code-Kommentare für das Schulungsprojekt

--- Assets/Scripts/Shelf.cs
+++ Assets/Scripts/Shelf.cs
@@ -7,12 +7,14 @@ using UnityEngine.UI;
 public class Shelf : MonoBehaviour
 {
 
+    // Ein Shelf soll seine Nummer (einstellbar) kennen und sein Label (um die Nummer anzuzeigen)
     public int shelfNumber;
-    //public int[] shelfNumbers
     public Text LabelText;
-    //public List<Text> labels;
 
-
+    // Ein Shelf soll außerdem ein Licht haben und drei Zustandsfarben
+    // Blau: Warte auf Input
+    // Rot: Input ungültig
+    // Grün: Input gültig
     public Light shelfLight;
     public Color correctColor;
     public Color emptyColor;
@@ -21,22 +23,30 @@ public class Shelf : MonoBehaviour
 
     public string shelfName;
     public bool isEmpty;
-	// Use this for initialization
+
     void Start ()
 	{
+        // Mit leerem Shelf starten.
+        // Und die Anzeige entsprechend der shelfNumber ändern.
 	    emptyShelf();
         LabelText.text = (shelfNumber+1).ToString();
 	}
 
     void OnTriggerEnter(Collider other)
     {
+        // Wenn irgendein Objekt in das Fach gelegt wird, wird der Status isEmpty auf falsch gesetzt
         isEmpty = false;
+
+        // Wenn der hinzugefügte Gegenstand kein Package ist (anhand des Tags)
         if (!other.CompareTag("Package"))
         {
             Debug.LogFormat("Nasty you! You tried to place object {0} in slot {1}", other.gameObject.name, shelfNumber+1);
             shelfLight.color = wrongColor;
             return;
         }
+
+        // Hier der Code wird (wg. return) nur ausgeführt, wenn es ein Package ist.
+        // Hier wird dann geprüft, ob es das richtige Package für das Fach ist oder nicht.
         Package p = other.GetComponent<Package>();
         if (p.targetShelfNumber == this.shelfNumber)
         {
@@ -57,7 +67,10 @@ public class Shelf : MonoBehaviour
 
     void CorrectPackage(GameObject package)
     {
+        // Wenn ein Package in das korrekte Fach gelegt wurde, soll das gameObject des Package nach einer halben Sekunde zerstört werden.
         Destroy(package, 0.5f);
+        // Außerdem soll mit der gleichen Verzögerung auch der isEmpty-Status wieder angepasst werden.
+        // Invoke ruft Methoden über den Namen auf und akzeptiert wie Destroy() einen Delay als zweites Argument
         Invoke("emptyShelf", 0.5f);
         shelfLight.color = correctColor;
     }
